f820662c2bdb9d55207c63bc2fccfa1a6c1cd539
gonzalo <gonzalo@gonzalo-sobremesa>
2022-09-27 20:33:12 +0200

    Proyecto Civitas ya comenzado, empiezo a desarrollar

---
+++ Civitas/src/civitas/Civitas.java
@@ -0,0 +1,36 @@
+package civitas;
+
+/**
+ *
+ * @author gonzalo
+ */
+
+enum TipoCasilla{
+    CALLE, 
+    SORPRESA, 
+    DESCANSO};
+
+enum TipoSorpresa{
+    PAGARCOBRAR, 
+    PORCASAHOTEL};
+
+enum EstadosJuego {
+  INICIO_TURNO,
+  DESPUES_AVANZAR,
+  DESPUES_COMPRAR,
+  DESPUES_GESTIONAR
+}
+        
+
+
+public class Civitas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // TODO code application logic here
+        
+    }
+    
+  
+    
+}
