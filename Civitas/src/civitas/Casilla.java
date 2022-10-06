8925a2c072cc1778ab59b3c563d1582c4f78f23d
gonzalo <gonzalo@gonzalo-sobremesa>
2022-10-06 21:52:33 +0200

    Clase jugador implementada

--- Civitas/src/civitas/Casilla.java
+++ Civitas/src/civitas/Casilla.java
@@ -5,12 +5,18 @@ public class Casilla {
     
     TipoCasilla tipo;
     
+    MazoSorpresas mazo;
+    
     private String Nombre;
     private float precioCompra, precioEdificar, precioBaseAlquiler;
 
     private static float FACTORALQUILERCALLE = 1.0f, FACTORALQUILERCASA = 1.0f,
     FACTORALQUILERHOTEL = 4.0f;
-    private static int numCasas, numHoteles;
+    private int numCasas, numHoteles;
+    
+    void init(){
+        
+    }
     
     Casilla (TipoCasilla unTipo, String unNombre, float unPrecioCompra, float unPrecioEdificar, float unPrecioAlquilerBase){
         tipo = unTipo;
@@ -20,8 +26,64 @@ public class Casilla {
         precioBaseAlquiler = unPrecioAlquilerBase;
     }
     
+    public int cantidadCasasHoteles(){
+        return numCasas + numHoteles;
+    }
+    
+    Casilla (String nombre){
+        tipo = TipoCasilla.DESCANSO;
+        this.Nombre = nombre;
+    }
+    
+    Casilla (String titulo, float precioCompra, float precioEdificar, float precioBaseAlquiler){
+        tipo = TipoCasilla.CALLE;
+        this.precioEdificar = precioEdificar;
+        this.precioBaseAlquiler = precioBaseAlquiler;
+    }
+    
+    Casilla (String nombre, MazoSorpresas mazo){
+        tipo = TipoCasilla.SORPRESA;
+        this.Nombre = nombre;
+        this.mazo = mazo;
+    }
+    
+    /*
+    
+    boolean comprar (Jugador jugador){
+        
+    }
+    
+    boolean construirCasa (Jugador jugador){
+    
+    }
+
+    boolean construirHotel (Jugador jugador){
+    
+    }
+
+*/
+    boolean derruirCasas (int numero, Jugador jugador){
+        
+    }
+    
     public float getPrecioAlquilerCompleto(){
         return precioBaseAlquiler * (FACTORALQUILERCASA + numCasas + numHoteles * FACTORALQUILERHOTEL);
+    }
+    
+    float getPrecioCompra(){
+        return precioCompra;
+    }
+    
+    float getPrecioEdificar(){
+        return precioEdificar;
+    }
+    
+    int getNumCasas(){
+        return numCasas;
+    }
+    
+    int getNumHoteles(){
+        return numHoteles;
     }
     
     public String toString(){
---
+++ Civitas/src/civitas/Jugador.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhSst/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package civitas;
+
+import java.util.ArrayList;
+
+public class Jugador implements Comparable<Jugador> {
+    protected static int CasasMax = 4;
+    protected static int CasasPorHotel = 4; 
+    protected static int HotelesMax = 4;
+    protected static float PasoPorSalida = 1000;
+
+    
+    private ArrayList<Casilla> propiedades;
+    
+    private int casillaActual;
+    private String nombre;
+    
+    private boolean puedeComprar;
+    private float saldo;
+    private static float SaldoInicial = 7500;
+    
+    Jugador(String nombre){
+        this.nombre = nombre;
+    }
+    
+    protected Jugador (Jugador otro){
+        this.casillaActual = otro.casillaActual;
+        this.nombre = otro.nombre;
+        this.puedeComprar = otro.puedeComprar;
+        this.saldo = otro.saldo;
+    }
+    
+    int cantidadCasasHoteles(){
+        int suma = 0;
+        for (int i=0; i<propiedades.size(); i++){
+            suma += propiedades.get(i).cantidadCasasHoteles();
+        }
+        return suma;
+    }
+    
+    public int compareTo (Jugador otro){
+        return Float.compare(saldo, otro.saldo);
+    }
+    
+    boolean comprar (Casilla titulo){
+        return true;
+    }    
+    
+    boolean construirCasa (int ip){
+        return true;
+    }
+    
+    boolean construirHotel (int ip){
+        return true;
+    }   
+    
+    boolean enBancarrota(){
+        boolean arruinado = false;
+        if (saldo < 0)
+            arruinado = true;
+        return arruinado;
+    }
+    
+    private boolean existeLaPropiedad(int ip){
+        boolean existe = false;
+        if (propiedades.size() > ip)
+            existe = true;
+        return existe;
+    }
+    
+    private static int getCasasMax(){
+        return CasasMax;
+    }
+    
+    static int getCasasPorHotel(){
+        return CasasPorHotel;
+    }
+    
+    int getCasillaActual(){
+        return casillaActual;
+    }
+    
+    private static int getHotelesMax(){
+        return HotelesMax;
+    }
+
+    protected String getNombre(){
+        return nombre;
+    }
+    
+    private static float getPremioPasoSalida(){
+        return PasoPorSalida;
+    }
+    
+    protected ArrayList<Casilla> getPropiedades(){
+        return propiedades;
+    }
+    
+    boolean getPuedeComprar(){
+        return puedeComprar;
+    }
+    
+    protected float getSaldo(){
+        return saldo;
+    }
+    
+    boolean modificarSaldo(float cantidad){
+        saldo = cantidad;
+        Diario.getInstance().ocurreEvento("El saldo fue modificado de " 
+                + saldo + " a " + cantidad);
+        return true;
+    }
+    
+    boolean moverACasilla (int numCasilla){
+        casillaActual = numCasilla;
+        puedeComprar = false;
+        Diario.getInstance().ocurreEvento("El jugador se ha movido de la casilla " 
+                + casillaActual + " a la casilla " + numCasilla);
+        return true;
+    }
+    
+    boolean paga (float cantidad){
+        return modificarSaldo(cantidad * -1);
+    }
+    
+    boolean pagaAlquiler (float cantidad){
+        return paga (cantidad);
+    }
+    
+    boolean recibe (float cantidad){
+        return modificarSaldo(cantidad);
+    }
+    
+    boolean pasaPorSalida(){
+        recibe(PasoPorSalida);
+        Diario.getInstance().ocurreEvento("Un jugador ha pasado por la casilla de salida y ha"
+                + " recibido la cantidad de " + PasoPorSalida);
+        return true;
+    }
+    
+    boolean puedeComprarCasilla(){
+        puedeComprar = true;
+        return puedeComprar;
+    }
+    
+    private boolean puedoGastar(float precio){
+        return saldo >= precio;
+    }
+    
+    //DUDA: ¿Cuál es el precio de edificar una casa?
+    private boolean puedoEdificarCasa(Casilla propiedad){
+        boolean puedoEdificar = false;
+        
+        if ( (puedoGastar(propiedad.getPrecioEdificar())) && (Jugador.CasasMax >= propiedad.getNumCasas())){
+            puedoEdificar = true;
+        }
+        return puedoEdificar;
+    }
+    
+    //DUDA: ¿Cuál es el precio de edificar un hotel?
+    private boolean puedoEdificarHotel(Casilla propiedad){
+        boolean puedoEdificar = false;
+        
+        if ( (puedoGastar(propiedad.getPrecioEdificar())) && (Jugador.HotelesMax >= propiedad.getNumHoteles())
+                && (Jugador.CasasPorHotel >= propiedad.getNumCasas()) ){
+            puedoEdificar = true;
+        }
+        return puedoEdificar;
+    }
+    
+    boolean tieneAlgoQueGestionar(){
+        boolean tieneAlgo = false;
+        if (propiedades.contains(this))
+            tieneAlgo = true;
+        return tieneAlgo;
+    }
+    
+    //DUDA: ¿Tengo que imprimir por pantalla las constantes?
+    public String toString(){
+        String puede;
+        if (puedeComprar)
+            puede = "SÍ";
+        else
+            puede = "NO";
+            
+        String cadena = "Número máximo de casas: " + CasasMax + ". Número máximo de casas por hotel: " + CasasPorHotel
+                + ". Casilla actual: " + casillaActual + ". Número máximo de hoteles: " + HotelesMax +
+                ". Nombre del jugador: " + nombre + ". Dinero por pasar por salida: " + PasoPorSalida +
+                ". Puede comprar: " + puede + ". Saldo: " + saldo + ". Saldo inicial: " + SaldoInicial;
+        return cadena;
+    }
+}
