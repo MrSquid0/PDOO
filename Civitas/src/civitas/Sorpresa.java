a5f21b8979a0968bc0561c502fb458e619941fb6
gonzalo <gonzalo@gonzalo-sobremesa>
2022-10-08 19:40:49 +0200

    Clase Sorpresa implementada

--- Civitas/src/civitas/Sorpresa.java
+++ Civitas/src/civitas/Sorpresa.java
@@ -5,6 +5,42 @@
 
 package civitas;
 
+import java.util.ArrayList;
+
 public class Sorpresa {
+    private String texto;
+    int valor;
+    TipoSorpresa tipo;
+    
+    Sorpresa(TipoSorpresa tipo, String texto, int valor){
+        this.tipo = tipo;
+        this.texto = texto;
+        this.valor = valor;
+    }
+    
+    void informe (int actual, ArrayList<Jugador> todos){
+        Diario.getInstance().ocurreEvento("Se está aplicando una sorpresa al jugador " + todos.get(actual).getNombre());
+    }
+    
+    void aplicarAJugador_pagarCobrar(int actual, ArrayList<Jugador> todos){
+        informe(actual, todos);
+        todos.get(actual).modificarSaldo(valor);
+    }
+    
+    void aplicarAJugador_porCasaHotel(int actual, ArrayList<Jugador> todos){
+        informe (actual, todos);
+        todos.get(actual).modificarSaldo(valor * todos.get(actual).cantidadCasasHoteles());        
+    }
+    
+    protected void aplicarAJugador(int actual, ArrayList<Jugador> todos){
+        if (tipo == TipoSorpresa.PAGARCOBRAR)
+            aplicarAJugador_pagarCobrar(actual, todos);
+        else
+            aplicarAJugador_porCasaHotel(actual, todos);
+    }
+    
+    public String toString(){
+        return texto;
+    }
 
 }
