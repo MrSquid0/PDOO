5f8db462237f8a3cf39c36a1ccd4015bd2e06e20
gonzalo <gonzalo@gonzalo-sobremesa>
2022-10-17 00:28:47 +0200

    Clases nuevas actualizadas

--- EjerciciosRuby/controlador_laberinto/controlador.rb
+++ EjerciciosRuby/controlador_laberinto/controlador.rb
@@ -1,29 +1,34 @@
 # encoding:utf-8
 
-class Controlador
-  def initialize (modelo)
-    @vidas = 0
-    @modelo = modelo
+module Controlador_laberinto
+
+  require_relative './estado_juego'
+  class Controlador
+    def initialize (modelo)
+      @vidas = 0
+      @modelo = modelo
+      @estado = Estado_juego.EN_ENTRADA_LABERINTO
+    end
+
+    def estado()
+
+    end
+
+    def entrar(vidas)
+
+    end
+
+    def intentar_avanzar()
+
+    end
+
+    def vidas()
+      @vidas
+    end
+
+    def habitacion_usuario
+
+    end
+
   end
-
-  def estado()
-
-  end
-
-  def entrar(vidas)
-
-  end
-
-  def intentar_avanzar()
-
-  end
-
-  def vidas()
-
-  end
-
-  def habitacion_usuario
-
-  end
-
 end
--- EjerciciosRuby/vista_laberinto.rb
+++ EjerciciosRuby/vista_laberinto.rb
@@ -1,19 +1,55 @@
 # encoding:utf-8
 
-class Vista_laberinto
-  def initialize (controlador)
-    @controlador = controlador
+module EjerciciosRuby
+  require_relative 'controlador_laberinto/controlador'
+  require_relative 'controlador_laberinto/estado_juego'
+  class Vista_laberinto
+    def initialize (controlador)
+      @controlador = controlador
+    end
+
+    def menu_usuario()
+      estado=@controlador.estado
+
+      case estado
+      when Estado_juego::EN_ENTRADA_LABERINTO
+        puts "Introduce el número de vidas para empezar el juego (1 a 10)"
+        num = gets.to_s.chomp
+
+        while num < 0 || num > 10 do
+          puts "Introduce el número de vidas para empezar el juego (1 a 10)"
+          num = gets.to_s.chomp
+        end
+
+      when Estado_juego::DENTRO_VIVO
+        informe_dentro @controlador, @vidas
+        puts "Pulsa la tecla ENTER para continuar"
+        puts controlador.intentar_avanzar
+
+      when Estado_juego::EN_SALIDA_LABERINTO
+        informe_final controlador.vidas
+        exit 0
+
+      when Estado_juego::DENTRO_MUERTO
+        informe_final controlador.vidas
+        exit 0
+      end
+
+      menu_usuario
+    end
+
+    def informe_dentro(habitacion, vidas)
+      puts "La habitación actual es " + habitacion.to_s + " y el número de vidas restantes es " + vidas.to_s
+    end
+
+    def informe_final(vidas)
+      puts "El número de vidas restantes es " + vidas.to_s
+    end
   end
 
-  def menu_usuario()
+  controlador = Controlador_laberinto::Controlador.new(0)
 
-  end
+  test = Vista_laberinto.new(controlador)
 
-  def informe_dentro(habitacion, vidas)
-
-  end
-
-  def informe_final(vidas)
-
-  end
+  test.menu_usuario
 end
