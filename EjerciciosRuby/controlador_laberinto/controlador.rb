1851a69ab60169292255a0f79f50317ce20c5a71
gonzalo <gonzalo@gonzalo-sobremesa>
2022-10-16 13:26:10 +0200

    Módulo Controlador_laberinto añadido

---
+++ EjerciciosRuby/controlador_laberinto/controlador.rb
@@ -0,0 +1,29 @@
+# encoding:utf-8
+
+class Controlador
+  def initialize (modelo)
+    @vidas = 0
+    @modelo = modelo
+  end
+
+  def estado()
+
+  end
+
+  def entrar(vidas)
+
+  end
+
+  def intentar_avanzar()
+
+  end
+
+  def vidas()
+
+  end
+
+  def habitacion_usuario
+
+  end
+
+end
---
+++ EjerciciosRuby/vista_laberinto.rb
@@ -0,0 +1,19 @@
+# encoding:utf-8
+
+class Vista_laberinto
+  def initialize (controlador)
+    @controlador = controlador
+  end
+
+  def menu_usuario()
+
+  end
+
+  def informe_dentro(habitacion, vidas)
+
+  end
+
+  def informe_final(vidas)
+
+  end
+end
