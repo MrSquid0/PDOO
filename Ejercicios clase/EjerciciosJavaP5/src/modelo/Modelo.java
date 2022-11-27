309c6052c2b092994fe4ee6878702eaba57a1850
Gonzalo <glopcas@krutt.org>
2022-11-27 19:58:53 +0100

    Práctica 4 terminada en Civitas

---
+++ Ejercicios clase/EjerciciosJavaP5/src/modelo/Modelo.java
@@ -0,0 +1,35 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Modelo {
+    private static final int PORDEFECTO=4;
+    private Random generador;
+    private ArrayList<Integer> coleccion1;
+    
+    public Modelo() {
+        generador=new Random();
+        coleccion1=new ArrayList<>();
+        coleccion1.add(PORDEFECTO);
+        
+    }
+
+    public ArrayList<Integer> getColeccion1() {
+        return coleccion1;
+    }
+
+    
+    public void añadeElemento(Integer i) {
+        coleccion1.add(i);
+    }
+    
+    public void quitaUno() {
+        if (coleccion1.size()>0) {
+            int pos=generador.nextInt(coleccion1.size());
+            coleccion1.remove(pos);
+        }
+    }
+    
+}
